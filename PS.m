e283fbc2cc1cbd9e6517c8911518431e202df547
Shayan Dodge <94797491+ShayanDodge@users.noreply.github.com>
2021-12-03 23:45:12 +0330

    Add files via upload

---
+++ PS.m
@@ -0,0 +1,15 @@
+% A Fast Algorithm for Particle Simulations
+clc
+clear all
+N=input('N=');
+n=log(N)./log(4);
+x=input('x=');
+y=input('y=');
+x_min=min(x);
+y_min=min(y);
+x_max=max(x);
+y_max=max(y);
+
+
+
+
