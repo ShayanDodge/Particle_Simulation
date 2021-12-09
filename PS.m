8b7df90cb88f07b00b19560ea094d5902ee1dfab
ShayanDodge <dodgeshayan@gmail.com>
2021-12-09 22:20:04 +0330

    Update PS.m

--- PS.m
+++ PS.m
@@ -26,6 +26,7 @@ y_domain=linspace(y_min,y_max,sqrt(4^n));
 z=zeros(sqrt(4^n),sqrt(4^n),n+1);% center at the mesh
 a=zeros(sqrt(4^n),sqrt(4^n),n+1);
 Q_cell=zeros(sqrt(4^n),sqrt(4^n),n+1);
+n_cell=zeros(sqrt(4^n),sqrt(4^n),n+1);
 for k=1:n+1
  for i=1:k
   for j=1:k   
@@ -39,12 +40,43 @@ z(i,j,k)= i.*(x_length./2) + j.*(y_length./2);
        y(charge)<=j.*y_length
 a(i,j,k)=a(i,j,k)-q(charge).*sqrt(x(charge).^2+y(charge)^2);
 Q_cell(i,j,k)=Q_cell(i,j,k)+q(charge);
+n_cell(i,j,k)=n_cell(i,j,k)+1;
     end
    end
   end
  end
 end
+
+for k=1:n
+  cells(k).z_i=zeros(n_cell(k),1);
+  cells(k).phi_z_i=zeros(n_cell(k),1);
+end
+
+for k=1:n+1
+ for i=1:k
+  for j=1:k   
+y_length=y_min+(y_max-y_min)./(2^(k-1));
+x_length=x_min+(x_max-x_min)./(2^(k-1));
+z(i,j,k)= i.*(x_length./2) + j.*(y_length./2);
+number=1;
+   for charge=1:N
+    if (i-1).*x_length<=x(charge) &...
+       x(charge)<=i.*x_length     &...
+       (j-1).*y_length<=y(charge) &...
+       y(charge)<=j.*y_length
+   
+cells(k).z_i(number)=sqrt(x(charge).^2+y(charge)^2);
+number=number+1;
+    end
+   end
+  end
+ end
+end
+
+
 phi=zeros(sqrt(4^n),sqrt(4^n),n+1);
+phi_i
+z_i
 psi=zeros(sqrt(4^n),sqrt(4^n),n+1);
 psi_p=zeros(sqrt(4^n),sqrt(4^n),n+1);
 %% ==============step 1======================= 
@@ -79,7 +111,7 @@ end
 % in the second inner loop to the centers of the...
 % box’s children, forming the initial expansion for...
 % the boxes at the next level.
-for k=1:n
+for k=1:n-1
     for i=1:(sqrt(4^(k-1)))
         for j=1:(sqrt(4^(k-1)))
             for ci=1:sqrt(4^(k-1))
@@ -87,7 +119,7 @@ for k=1:n
                    if cj==j & ci==i 
                    else
                 psi(i,j,k)=psi(i,j,k)+...
-                b_0(ci,cj,k).*log(z(ci,cj,k))+b_1(ci,cj,k).*z(ci,cj,k);
+                -a(ci,cj,k)./z(ci,cj,k)+Q(ci,cj,k).*log(z(ci,cj,k))+(a(ci,cj,k)./z(ci,cj,k).^2-Q./z(ci,cj,k)).*z(ci,cj,k);
                    end
                 end
             end
@@ -98,11 +130,37 @@ for k=1:n
      for ci=1:(sqrt(4^(kp1-1)))
         for cj=1:(sqrt(4^(kp1-1)))
                 psi_p(ci,cj,kp1)=psi_p(ci,cj,kp1)+...
-                    psi(ceil(ci./sqrt(4^(n-(kp1-1)))),ceil(cj./sqrt(4^(n-(kp1-1)))),k).*z_0(ci,cj,kp1);
+                    psi(ceil(ci./sqrt(4^(n-(kp1-1)))),ceil(cj./sqrt(4^(n-(kp1-1)))),k).*(Q-a(ci,cj,kp1).*z(ci,cj,kp1)).*z(ci,cj,kp1);
         end
      end 
 end
 %% ==============step 4=======================
+% Compute interactions at finest mesh level.
+    for i=1:(sqrt(4^(n)))
+        for j=1:(sqrt(4^(n)))
+            for ci=1:sqrt(4^(n))
+                for cj=1:sqrt(4^(n))
+                   if cj==j & ci==i 
+                   else
+                psi(i,j,k)=psi(i,j,k)+...
+                -a(ci,cj,k)./z(ci,cj,k)+Q(ci,cj,k).*log(z(ci,cj,k))+(a(ci,cj,k)./z(ci,cj,k).^2-Q./z(ci,cj,k)).*z(ci,cj,k);
+                   end
+                end
+            end
+        end
+    end
+%% ==============step 5=======================
+% Evaluate local expansions at particle positions.
+for k=1:4^n
+    for num_i=1:n_cell(k)
+        for num_j=1:n_cell(k)
+cells(k).phi_z_i(num_i)=-log(cells(k).z_i(num_j)-cells(k).z_i(num))
+        end
+    end
+end
+%% ==============step 6=======================
+
+
 
 
 
