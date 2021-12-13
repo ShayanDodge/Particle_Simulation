a0f4da3748d8be5d33bde8ea43b7b4f521f5aa4f
ShayanDodge <dodgeshayan@gmail.com>
2021-12-13 14:24:57 +0330

    Update PS.m

--- PS.m
+++ PS.m
@@ -90,57 +90,63 @@ cell_number=cell_number+1;
   end
  end
 %% ==============step 1====================================================
-% From multipole expansions of potential field
-% due to particles in each box about the box 
-% center at the finest mesh level
+% From multipole expansions of potential field due to particles in each box
+% about the box center at the finest mesh level
 phi(:,:,n+1) = Q_cell(:,:,n+1).*log(z(:,:,n+1))+(a_1(:,:,n+1)./z(:,:,n+1));
 %% ==============step 2====================================================
-% Form multipole expansions about the centers
-% of all boxes at all coarser mesh levels, each
-% expansion representing the potential field
-% due to all particles contained in one box.
+% Form multipole expansions about the centers of all boxes at all coarser 
+% mesh levels, each expansion representing the potential field due to all
+% particles contained in one box.
 for k=1:n
     for i=1:(sqrt(4^(k-1)))
         for j=1:(sqrt(4^(k-1)))
-            for ci=i.*sqrt(4^(n-(k-1))):(i-1).*sqrt(4^(n-(k-1)))+1
-                for cj=j.*sqrt(4^(n-(k-1))):(j-1).*sqrt(4^(n-(k-1)))+1
+            for ci=(i-1).*sqrt(4^(n-(k-1)))+1:(i).*sqrt(4^(n-(k-1)))
+                for cj=(j-1).*sqrt(4^(n-(k-1)))+1:(j).*sqrt(4^(n-(k-1)))
                 phi(i,j,k)=phi(i,j,k)+...
-                Q_cell(ci,cj,k).*log(z(ci,cj,k))+Q_cell(ci,cj,k).*z(ci,cj,k);
+                Q_cell(ci,cj,n+1).*log(z(i,j,k))-Q_cell(ci,cj,n+1).*z(ci,cj,n+1)./z(i,j,k);
                 end
             end
         end
     end
 end
 %% ==============step 3====================================================
-% Form a local expansion about the center of each...
-% box at each mesh level l<=n-1. This local expansion...
-% describes the field due to all particles in the...
-% system that are not contained in the current box...
-% or its nearest neighbors. Once the local expansion...
-% is obtained for a given box, it is shifted,...
-% in the second inner loop to the centers of the...
-% box’s children, forming the initial expansion for...
-% the boxes at the next level.
-for k=1:n-1
+% Form a local expansion about the center of each box at each mesh level 
+% l<=n-1. This local expansion describes the field due to all particles in
+% the system that are not contained in the current box or its nearest 
+% neighbors. Once the local expansion is obtained for a given box, it is 
+% shifted, in the second inner loop to the centers of the box’s children, 
+% forming the initial expansion for the boxes at the next level.
+for k=1:n
     for i=1:(sqrt(4^(k-1)))
         for j=1:(sqrt(4^(k-1)))
             for ci=1:sqrt(4^(k-1))
                 for cj=1:sqrt(4^(k-1))
                    if cj==j & ci==i 
                    else
-                psi(i,j,k)=psi(i,j,k)+...
-                -a_1(ci,cj,k)./z(ci,cj,k)+Q(ci,cj,k).*log(z(ci,cj,k))+(a_1(ci,cj,k)./z(ci,cj,k).^2-Q./z(ci,cj,k)).*z(ci,cj,k);
+psi(i,j,k)=psi(i,j,k)+...
+-a_1(ci,cj,k)./z(ci,cj,k)+Q_cell(ci,cj,k).*log(-z(ci,cj,k))+...
+(a_1(ci,cj,k)./z(ci,cj,k).^2-Q_cell(ci,cj,k)./z(ci,cj,k)).*z(i,j,k);
                    end
                 end
             end
         end
     end
- psi_p(i,j,k)=psi(i,j,k);
+ psi_p(:,:,1:n)=psi(:,:,1:n);
+psi_p(1,:,:)=0;
+psi_p(end,:,:)=0;
+psi_p(:,1,:)=0;
+psi_p(:,end,:)=0;
  kp1=k+1;
      for ci=1:(sqrt(4^(kp1-1)))
         for cj=1:(sqrt(4^(kp1-1)))
-                psi_p(ci,cj,kp1)=psi_p(ci,cj,kp1)+...
-                    psi(ceil(ci./sqrt(4^(n-(kp1-1)))),ceil(cj./sqrt(4^(n-(kp1-1)))),k).*(Q-a_1(ci,cj,kp1).*z(ci,cj,kp1)).*z(ci,cj,kp1);
+psi_p(ci,cj,kp1)=psi_p(ci,cj,kp1)+psi(ceil(ci./sqrt(4^(n-(kp1-1)))),...
+ceil(cj./sqrt(4^(n-(kp1-1)))),k).*(a_1(ceil(ci./sqrt(4^(n-(kp1-1)))),...
+ceil(cj./sqrt(4^(n-(kp1-1)))),k).*(-z(ceil(ci./sqrt(4^(n-(kp1-1)))),...
+ceil(cj./sqrt(4^(n-(kp1-1)))),k)+z(ci,cj,kp1)));
+psi_p(1,:,:)=0;
+psi_p(end,:,:)=0;
+psi_p(:,1,:)=0;
+psi_p(:,end,:)=0;
         end
      end 
 end
@@ -152,8 +158,9 @@ end
                 for cj=1:sqrt(4^(n))
                    if cj==j & ci==i 
                    else
-                psi(i,j,k)=psi(i,j,k)+...
-                -a_1(ci,cj,k)./z(ci,cj,k)+Q(ci,cj,k).*log(z(ci,cj,k))+(a_1(ci,cj,k)./z(ci,cj,k).^2-Q./z(ci,cj,k)).*z(ci,cj,k);
+psi(i,j,n+1)=psi(i,j,n+1)+...
+-a_1(ci,cj,n+1)./z(ci,cj,n+1)+Q_cell(ci,cj,n+1).*log(-z(ci,cj,n+1))+...
+(a_1(ci,cj,n+1)./z(ci,cj,n+1).^2-Q_cell(ci,cj,n+1)./z(ci,cj,n+1)).*z(i,j,n+1);                   
                    end
                 end
             end
