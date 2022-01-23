234f7bb29402ddb65c78404fadda46a0d3a7df4e
ShayanDodge <dodgeshayan@gmail.com>
2022-01-23 15:50:42 +0330

    Update PS.m

--- PS.m
+++ PS.m
@@ -89,21 +89,22 @@ end
 end
 for k=1:4^(n)
   cells(k).z_i=zeros(N_cell(k),1);% location of each particles inside each box
-  cells(k).phi_z_i=zeros(N_cell(k),1);% the multipole expansion at each particles location
+  cells(k).phi_z_i=zeros(N_cell(k),1);% the direct term at each particles location
+  cells(k).psi_z_i=zeros(N_cell(k),1);% the far field term at each particles location
   cells(k).total_phi_z_i=zeros(N_cell(k),1);% the total potential at each particles location
 end
 % This loop detects location of each particles inside each box (z=x^2+y^2).
 cell_number=1;
  for i=1:2^(n)
   for j=1:2^(n) 
-y_length=y_min+(y_max-y_min)./(2^(k-1));
-x_length=x_min+(x_max-x_min)./(2^(k-1));
+y_length=(y_max-y_min)./(2^(n));
+x_length=(x_max-x_min)./(2^(n));
 charge_number=1;
    for charge=1:N
-    if (i-1).*x_length<=x(charge) &...
-       x(charge)<=i.*x_length     &...
-       (j-1).*y_length<=y(charge) &...
-       y(charge)<=j.*y_length
+    if x_min+(i-1).*x_length<=x(charge) &...
+       x(charge)<=x_min+i.*x_length     &...
+       y_min+(j-1).*y_length<=y(charge) &...
+       y(charge)<=y_min+j.*y_length
 cells(cell_number).z_i(charge_number)=sqrt(x(charge).^2+y(charge)^2);
 charge_number=charge_number+1; 
     end
@@ -114,7 +115,7 @@ cell_number=cell_number+1;
 %% ==============step 1====================================================
 % From multipole expansions of potential field due to particles in each box
 % about the box center at the finest mesh level
-phi(:,:,n+1) = Q_cell(:,:,n+1).*log(z(:,:,n+1))+(a_1(:,:,n+1)./z(:,:,n+1));
+phi(:,:,n+1) = Q_cell(:,:,n+1).*log10(z(:,:,n+1))+(a_1(:,:,n+1)./z(:,:,n+1));
 %% ==============step 2====================================================
 % Form multipole expansions about the centers of all boxes at all coarser 
 % mesh levels, each expansion representing the potential field due to all
@@ -125,7 +126,8 @@ for k=1:n
             for ci=(i-1).*sqrt(4^(n-(k-1)))+1:(i).*sqrt(4^(n-(k-1)))
                 for cj=(j-1).*sqrt(4^(n-(k-1)))+1:(j).*sqrt(4^(n-(k-1)))
                 phi(i,j,k)=phi(i,j,k)+...
-                Q_cell(ci,cj,n+1).*log(z(i,j,k))-Q_cell(ci,cj,n+1).*z(ci,cj,n+1)./z(i,j,k);
+                Q_cell(ci,cj,k+1).*log10(z(i,j,k))-Q_cell(ci,cj,k+1).*...
+                z(ci,cj,k+1)./z(i,j,k);
                 end
             end
         end
@@ -143,22 +145,40 @@ for k=1:n
         for j=1:(sqrt(4^(k-1)))
             for ci=1:sqrt(4^(k-1))
                 for cj=1:sqrt(4^(k-1))
-                   if cj==j & ci==i 
+                   if ceil(ci./4)==ceil(i./4)-1 & ceil(cj./4)==ceil(j./4)  |...
+                      ceil(ci./4)==ceil(i./4)   & ceil(cj./4)==ceil(j./4)  |...
+                      ceil(ci./4)==ceil(i./4)+1 & ceil(cj./4)==ceil(j./4  )|...
+                      ceil(ci./4)==ceil(i./4)-1 & ceil(cj./4)==ceil(j./4)+1|...
+                      ceil(ci./4)==ceil(i./4)   & ceil(cj./4)==ceil(j./4)+1|...
+                      ceil(ci./4)==ceil(i./4)+1 & ceil(cj./4)==ceil(j./4)+1|...
+                      ceil(ci./4)==ceil(i./4)-1 & ceil(cj./4)==ceil(j./4)-1|...
+                      ceil(ci./4)==ceil(i./4)   & ceil(cj./4)==ceil(j./4)-1|...
+                      ceil(ci./4)==ceil(i./4)+1 & ceil(cj./4)==ceil(j./4)-1
+                   if ci==i   & cj==j     |...
+                      ci==i   & cj==j-1   |...
+                      ci==i   & cj==j+1   |...
+                      ci==i+1 & cj==j     |...
+                      ci==i+1 & cj==j-1   |... 
+                      ci==i+1 & cj==j+1   |...
+                      ci==i-1 & cj==j     |...
+                      ci==i-1 & cj==j-1   |... 
+                      ci==i-1 & cj==j+1
                    else
 psi(i,j,k)=psi(i,j,k)+...
--a_1(ci,cj,k)./z(ci,cj,k)+Q_cell(ci,cj,k).*log(-z(ci,cj,k))+...
+-a_1(ci,cj,k)./z(ci,cj,k)+Q_cell(ci,cj,k).*log10(-z(ci,cj,k))+...
 (a_1(ci,cj,k)./z(ci,cj,k).^2-Q_cell(ci,cj,k)./z(ci,cj,k)).*z(i,j,k);
+                   end
                    end
                 end
             end
         end
     end
- psi_p(:,:,1:n)=psi(:,:,1:n);
+psi_p(:,:,k)=psi_p(:,:,k)+psi(:,:,k);
 psi_p(1,:,:)=0;
 psi_p(end,:,:)=0;
 psi_p(:,1,:)=0;
 psi_p(:,end,:)=0;
- kp1=k+1;
+kp1=k+1;
      for ci=1:(sqrt(4^(kp1-1)))
         for cj=1:(sqrt(4^(kp1-1)))
 psi_p(ci,cj,kp1)=psi_p(ci,cj,kp1)+psi(ceil(ci./sqrt(4^(n-(kp1-1)))),...
@@ -174,15 +194,35 @@ psi_p(:,end,:)=0;
 end
 %% ==============step 4====================================================
 % Compute interactions at finest mesh level.
+% particles that there are in the interaction list will be defined
     for i=1:(sqrt(4^(n)))
         for j=1:(sqrt(4^(n)))
             for ci=1:sqrt(4^(n))
                 for cj=1:sqrt(4^(n))
-                   if cj==j & ci==i 
+                   if ceil(ci./4)==ceil(i./4)-1 & ceil(cj./4)==ceil(j./4)  |...
+                      ceil(ci./4)==ceil(i./4)   & ceil(cj./4)==ceil(j./4)  |...
+                      ceil(ci./4)==ceil(i./4)+1 & ceil(cj./4)==ceil(j./4  )|...
+                      ceil(ci./4)==ceil(i./4)-1 & ceil(cj./4)==ceil(j./4)+1|...
+                      ceil(ci./4)==ceil(i./4)   & ceil(cj./4)==ceil(j./4)+1|...
+                      ceil(ci./4)==ceil(i./4)+1 & ceil(cj./4)==ceil(j./4)+1|...
+                      ceil(ci./4)==ceil(i./4)-1 & ceil(cj./4)==ceil(j./4)-1|...
+                      ceil(ci./4)==ceil(i./4)   & ceil(cj./4)==ceil(j./4)-1|...
+                      ceil(ci./4)==ceil(i./4)+1 & ceil(cj./4)==ceil(j./4)-1
+                   if ci==i   & cj==j     |...
+                      ci==i   & cj==j-1   |...
+                      ci==i   & cj==j+1   |...
+                      ci==i+1 & cj==j     |...
+                      ci==i+1 & cj==j-1   |... 
+                      ci==i+1 & cj==j+1   |...
+                      ci==i-1 & cj==j     |...
+                      ci==i-1 & cj==j-1   |... 
+                      ci==i-1 & cj==j+1
                    else
 psi(i,j,n+1)=psi(i,j,n+1)+...
 -a_1(ci,cj,n+1)./z(ci,cj,n+1)+Q_cell(ci,cj,n+1).*log(-z(ci,cj,n+1))+...
-(a_1(ci,cj,n+1)./z(ci,cj,n+1).^2-Q_cell(ci,cj,n+1)./z(ci,cj,n+1)).*z(i,j,n+1);                   
+(a_1(ci,cj,n+1)./z(ci,cj,n+1).^2-Q_cell(ci,cj,n+1)./z(ci,cj,n+1)).*z(i,j,n+1); 
+psi_p(i,j,n+1)=psi_p(i,j,n+1)+psi(i,j,n+1);
+                       end
                    end
                 end
             end
@@ -190,15 +230,13 @@ psi(i,j,n+1)=psi(i,j,n+1)+...
     end
 %% ==============step 5====================================================
 % Evaluate local expansions at particle positions.
-for k=1:4^n
-    for num_i=1:N_cell(k)
-        for num_j=1:N_cell(k)
-            if num_i~=num_j
-cells(k).phi_z_i(num_i)=cells(k).phi_z_i(num_i)-...
-log(cells(k).z_i(num_j)-cells(k).z_i(num_i));
-            end
-        end
-    end
+for i=1:2^n
+ for j=1:2^n
+     k=create_linear_index_list(z,i,j,n+1)-(n.*(2^n).^2);
+   for num_i=1:N_cell(k)
+cells(k).psi_z_i(num_i)=psi_p(i,j,n+1);
+   end
+ end
 end
 %% ==============step 6====================================================
 % Compute potential (or force) due to nearest neighbors directly.
@@ -220,16 +258,11 @@ log(cells(k).z_i(num_i)-cells(k_n).z_i(num_j));
   end  
  end
 end
-
 %% ==============step 7====================================================
 
-for i=1:2^n
- for j=1:2^n
-     k=create_linear_index_list(z,i,j,n+1)-(n.*(2^n).^2);
+for k=1:4^n
    for num_i=1:N_cell(k)
-cells(k).total_phi_z_i(num_i)=cells(k).phi_z_i(num_i)+phi(i,j,n+1)+...
-psi(i,j,n+1)+psi_p(i,j,n+1);
+cells(k).total_phi_z_i(num_i)=cells(k).phi_z_i(num_i)+cells(k).psi_z_i(num_i);
    end
- end
 end
 end
